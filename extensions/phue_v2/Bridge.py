564756136a5d7da741c727e8067cf851b0b08322
paddy-shrp <patrickschroeppel@gmail.com>
2024-05-02 13:25:40 +0200

    Phue_v2 Rewrite

--- extensions/phue_v2/Bridge.py
+++ extensions/phue_v2/Bridge.py
@@ -1,8 +1,10 @@
 import urllib3
-import requests as rq
 from urllib3.exceptions import InsecureRequestWarning
+import requests as rq
+from threading import Thread
+import numpy as np
+
 import utils.credentials as credentials
-import json
 
 urllib3.disable_warnings(InsecureRequestWarning)
 CREDENTIALS_PATH = "phue_bridge.json"
@@ -18,30 +20,47 @@ class Bridge:
             cred = credentials.get_credentials_json(CREDENTIALS_PATH)
         self.username = cred["username"]
         self.clientkey = cred["clientkey"]
-        self.get_lights()
-        # self.set_light_state(9, cmds={"on": {"on": True}})
 
     def get_lights(self):
         path = self.general_path + "light"
-        data = self.execute_request(path)["data"]
-        with open("./extensions/tests/lights.json", "w") as file:
-            json.dump(data, file)
-        
+        lights_raw = self.execute_request(path)["data"]
         lights = []
-        for light_raw in data:
-            light = {}
-            light["id"] = light_raw["id"]
-            light["name"] = light_raw["metadata"]["name"]
-            lights.append(light)
-        print(lights)
-        
+        for light_raw in lights_raw:
+            lights.append(self.get_processed_light(light_raw))
+        return lights
 
-    def set_light_state(self, ids, cmds, transitiontime = None):
-        if type(ids) == int: ids = [ids]
+    def get_light(self, id):
+        path = self.general_path + f"light/{id}"
+        light_raw = self.execute_request(path)["data"][0]
+        return self.get_processed_light(light_raw)
+
+    def get_processed_light(self, light_raw):
+        light = {}
+        light["id"] = light_raw["id"]
+        light["name"] = light_raw["metadata"]["name"]
+        light["on"] = light_raw["on"]["on"]
+        if "dimming" in light_raw:
+            light["brightness"] = light_raw["dimming"]["brightness"]
+        return light
+
+    def set_light_state(self, ids, on = None, bri = None, color = None, transitiontime = None):
+        if type(ids) == str: ids = [ids]
+
+        cmds = {}
+        if on is not None:
+            cmds["on"] = {"on": on}
+        if bri is not None:
+            cmds["dimming"] = {"brightness": bri}
+        if color is not None:
+            color_xy = self.convert_rbg_to_xy(*color)
+            cmds["color"] = {"xy": {"x": color_xy[0], "y": color_xy[1]}}
 
         for id in ids:
             path = self.general_path + f"light/{id}"
-            print(self.execute_request(path, cmds, "PUT"))        
+            try:
+                Thread(target=self.execute_request, args=(path, cmds, "PUT")).start()  
+            except:
+                pass      
 
     def execute_request(self, url, payload={}, method="GET"):
         response = None
@@ -55,7 +74,6 @@ class Bridge:
         return response.json()
 
     def generate_client_key(self):
-        response = rq.post(f"https://{self.host}/api", json={"devicetype": "phueBridge", "generateclientkey": True}, verify=False)
         input("Press any button after the link button has been pressed")
         response = rq.post(f"https://{self.host}/api", json={"devicetype": "phueBridge", "generateclientkey": True}, verify=False)
         try: 
@@ -65,6 +83,19 @@ class Bridge:
                                         "clientkey": data["clientkey"]})
         except:
             print("Please press the link button!")
+            self.generate_client_key()
 
+    def convert_rbg_to_xy(self, r, g, b):
+        rgb_vector = np.array([r, g, b]) / 255.0
+        transformation_matrix = np.array([
+            [0.4124564, 0.3575761, 0.1804375],
+            [0.2126729, 0.7151522, 0.0721750],
+            [0.0193339, 0.1191920, 0.9503041]
+        ])
+        color_vector = transformation_matrix @ rgb_vector
+        color_sum = color_vector.sum()
+        if color_sum == 0: return [0.35, 0.35] 
+        color_xy = (color_vector / color_sum)[:2]
+        return color_xy
 
-b = Bridge("192.168.178.28")
+# https://viereck.ch/hue-xy-rgb/
--- extensions/phue_v2/phue_v2.py
+++ extensions/phue_v2/phue_v2.py
@@ -5,30 +5,17 @@ from utils.decorators import *
 from utils.objects.extension import Extension
 from extensions.phue_v2.Bridge import Bridge
 
-# Hue: 7227
-# Sat: 223
-# ct: 494
-
 class PHue(Extension):
     def __init__(self, host=""):
         default_settings = {"IP": host}
         settings = self.get_extension_settings(default_settings)
-        self.hue = Bridge(settings["IP"])
+        self.bridge = Bridge(settings["IP"])
 
-    def set_light_state(self, ids, on = None, bri = None, hue = None, sat = None, transitiontime = None):
-        cmds = {}
-        if on is not None:
-            cmds["on"] = on
-        if bri is not None:
-            cmds["bri"] = bri
-        if hue is not None:
-            cmds["hue"] = hue
-        if sat is not None:
-            cmds["sat"] = sat
+    def set_light_state(self, ids, on = None, bri = None, color = None, transitiontime = None):
         if transitiontime is not None:
             if transitiontime < 4 and on is True and bri is None:
-                cmds["bri"] = 255
-        self.hue.bridge.set_light(ids)
+                bri = 255
+        self.bridge.set_light_state(ids, on, bri, color, transitiontime)
 
     @include_put
     def on(self, ids, transitiontime:int = None):
@@ -48,8 +35,7 @@ class PHue(Extension):
 
     @include_put
     def set_to_base_color(self, ids, transitiontime:int = 4):
-        self.bridge.set_light(
-            ids, self.get_base_color_command(255, transitiontime))
+        return self.set_light_state(ids, bri=255, color=[255, 255, 255], transitiontime=transitiontime)
 
     @include_put
     def start_flicker(self, ids, duration:int = 3, frequency:int = 1):
@@ -74,13 +60,12 @@ class PHue(Extension):
     def __color_switch(self, ids, duration, transitiontime, waitTime):
         startTime = time()
         while (time() - startTime) < duration:
-            self.set_light_state(ids, bri=255, hue=randint(0, 65535), sat=randint(
-                0, 255), transitiontime=transitiontime)
+            self.set_light_state(ids, bri=255, color=self.get_random_color(), transitiontime=transitiontime)
             sleep(waitTime)
-        self.bridge.set_light(ids, self.get_base_color_command(0))
+        self.set_to_base_color(ids)
 
     @include_put
-    async def start_pulse(self, ids, duration:int = 3, frequency:float = 0.5, stepCount:int = 10, minBri:int = 100, maxBri:int = 255):
+    def start_pulse(self, ids, duration:int = 3, frequency:float = 0.5, stepCount:int = 10, minBri:int = 100, maxBri:int = 255):
         Thread(target=self.__pulse, args=(
             ids, duration, frequency, stepCount, minBri, maxBri)).start()
 
@@ -98,21 +83,14 @@ class PHue(Extension):
                 sleep(stepTime)
         self.temp_off(ids)
 
-    def get_base_color_command(self, bri=255, transitiontime=4):
-        cmds = {}
-        if bri != None:
-            cmds["bri"] = bri
-        cmds["ct"] = 343
-        cmds["hue"] = 8632
-        cmds["sat"] = 117
-        cmds["transitiontime"] = transitiontime
-        return cmds
+    @include_get
+    def get_lights(self):
+        return self.bridge.get_lights()
 
     @include_get
-    async def get_lights(self):
-        return self.bridge.lights
-
-    @include_get
-    async def get_data(self):
+    def get_data(self):
         data = {}
         return data
+    
+    def get_random_color(self):
+        return [randint(0, 255), randint(0, 255), randint(0, 255)]
